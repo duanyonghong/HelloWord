0a95f678239564cb9d0125ccf33ed521d70ff982
duanyonghong <53213255+duanyonghong@users.noreply.github.com>
2020-08-26 18:22:20 +0800

    gitbug modify dyh 修改
    描述信息

--- hello1/src/hello/HelloWord.java
+++ hello1/src/hello/HelloWord.java
@@ -14,7 +14,7 @@ public class HelloWord {
 
 
     public static void main(String[] args) {
-        System.out.println("hello word");
+        System.out.println("hello word github modify");
         User user = new User();
 
     }
