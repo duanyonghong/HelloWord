aa9c6cf40501a506ff0d0abdfb7e2df279fcf422
dyh <dyh@127.0.0.1>
2020-08-27 17:18:22 +0800

    修改dyh 2020.08.27

---
+++ helloweb/src/test/Test1.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author dyh
+ * @date 2020.08.27 下午 12:45
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("我要看乱码");
+    }
+}
---
+++ helloweb/src/web/filter/HelloFilter.java
@@ -0,0 +1,38 @@
+package web.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * @author dyh
+ * @date 2020.08.27 上午 10:20
+ */
+@WebFilter(filterName = "HelloFilter",urlPatterns = "/*")
+public class HelloFilter implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("...................1: 过滤器 (初始化) ...................");
+        System.out.println("eeee你好");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("................... 过滤器   (前) ...................");
+
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8"); //返回页面数据乱码处理
+
+
+        chain.doFilter(request, response);
+        System.out.println("................... 过滤器   (后) ...................");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("................... 过滤器 (销毁) ...................");
+    }
+
+}
